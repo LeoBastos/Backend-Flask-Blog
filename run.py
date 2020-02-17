30fbda681c610e1e658b0e8931d90b63bfc35372
Leo Bastos <57710891+LeoBastos@users.noreply.github.com>
2020-02-17 14:14:22 -0300

    Add files via upload

---
+++ run.py
@@ -0,0 +1,10 @@
+from flaskblog import app
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+#pip install flask_sqlalchemy
+#pip install flask_bcrypt
+#pip install flask_login
+#pip install Pillow
+#pip install flask_wtf
